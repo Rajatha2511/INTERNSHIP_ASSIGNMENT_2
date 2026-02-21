cb423f2ac0fea29f0cce0d3f5d6399c043f2850b
Rajatha2511 <rajathavinyas16@gmail.com>
2026-02-21 19:34:32 +0530

    Initial Commit

---
+++ student_bio_card.py
@@ -0,0 +1,20 @@
+#taking user input
+name=input ("Enter Your Name: ")
+age=int(input("Enter Your age: "))
+course=input("Enter Your Course name: ")
+college=input("Enter Your College name: ")
+email=input("Enter Your email_ID: ")
+#Printing BioCard 
+print("\n"+"*" *40)
+print("         STUDENT BIO CARD")
+print("*" *40)
+print("Name  :",name)
+print("Age  :",age)
+print("Course  :",course)
+print("College   :",college)
+print("Email  :",email)
+print("*" *40)
+
+
+
+
